fd4b7c579b4b02b345f16ad109127bafd237c5a0
franck-armand <52790721+franck225-coder@users.noreply.github.com>
2022-12-12 16:58:13 +0800

    adaboost final implementation

---
+++ Adaboost Project/adaBoost_model_implementation.py
@@ -0,0 +1,92 @@
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+import numpy as np
+
+class AdaBoost:
+    """ AdaBoost enemble classifier from scratch """
+
+    def __init__(self):
+        self.stumps = None
+        self.stump_weights = None
+        self.errors = None
+        self.sample_weights = None
+
+    def _check_X_y(self, X, y):
+        """ Validate assumptions about format of input data"""
+        assert set(y) == {0, 1}, 'Response variable must be ±1'
+        return X, y
+
+def fit(self, X: np.ndarray, y: np.ndarray, iters: int):
+    """ Fit the model using training data """
+
+    X, y = self._check_X_y(X, y)
+    n = X.shape[0]
+
+    # init numpy arrays
+    self.sample_weights = np.zeros(shape=(iters, n))
+    self.stumps = np.zeros(shape=iters, dtype=object)
+    self.stump_weights = np.zeros(shape=iters)
+    self.errors = np.zeros(shape=iters)
+
+    # initialize weights uniformly
+    self.sample_weights[0] = np.ones(shape=n) / n
+
+    for t in range(iters):
+        # fit  weak learner
+        curr_sample_weights = self.sample_weights[t]
+        stump = DecisionTreeClassifier(max_depth=1, max_leaf_nodes=2)
+        stump = stump.fit(X, y, sample_weight=curr_sample_weights)
+
+        # calculate error and stump weight from weak learner prediction
+        stump_pred = stump.predict(X)
+        err = curr_sample_weights[(stump_pred != y)].sum()# / n
+        stump_weight = np.log((1 - err) / err) / 2
+
+        # update sample weights
+        new_sample_weights = (
+            curr_sample_weights * np.exp(-stump_weight * y * stump_pred)
+        )
+        
+        new_sample_weights /= new_sample_weights.sum()
+
+        # If not final iteration, update sample weights for t+1
+        if t+1 < iters:
+            self.sample_weights[t+1] = new_sample_weights
+
+        # save results of iteration
+        self.stumps[t] = stump
+        self.stump_weights[t] = stump_weight
+        self.errors[t] = err
+
+    return self
+
+# Making predictions
+def predict(self, X):
+    """ Make predictions using already fitted model """
+    stump_preds = np.array([stump.predict(X) for stump in self.stumps])
+    return np.sign(np.dot(self.stump_weights, stump_preds))
+
+
+data_train = "../classification_dataset/classification_train.csv"
+data_train = pd.read_csv(data_train)
+X = np.array(data_train.iloc[:,0:2])
+y = np.array(data_train.iloc[:,-1])
+
+# assign our individually defined functions as methods of our classifier
+AdaBoost.fit = fit
+AdaBoost.predict = predict
+
+clf = AdaBoost().fit(X, y, iters=20)
+
+train_err = (clf.predict(X) != y).mean()
+print(f'Train error: {train_err:.1%}')
+
+# testing on test set and saving test prediction
+data_test = "../classification_dataset/classification_test.csv"
+data = pd.read_csv(data_test)
+X = np.array(data[['x_1','x_2']])
+
+Y = clf.predict(X)
+predicted_Y = pd.DataFrame(Y)
+print('=============Saving prediction===================================="')
+predicted_Y.to_csv('predicted_adaBoost1.csv', index = False, header = False)
